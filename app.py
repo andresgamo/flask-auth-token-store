063db1503e3937ecb1cf78753bedc11420444358
Andrés García Molina <andresgarciamolina3@gmail.com>
2024-08-13 21:58:18 -0500

    Adding UserLogin class and refactor dependency injection inside data validation and UserRegister

--- app.py
+++ app.py
@@ -29,30 +29,25 @@ class DatabaseManager:
 
 
 class DataValidation:
-    def __init__(self, db_manager: DatabaseManager):
+    def __init__(self, db_manager: DatabaseManager, bcrypt: Bcrypt):
         self.db_manager = db_manager
+        self.bcrypt = bcrypt
 
     def data_complete(self, data: dict) -> bool:
         """Validates user and password have been sent"""
         return bool(data.get("username")) and bool(data.get("password"))
 
-    def user_exists(self, username: str) -> bool:
-        """Verify in the db if user exist"""
-        return bool(self.db_manager.find_user_by_username(username))
-
     def verify_password(self, username: str, password: str) -> bool:
         """Verify if the provided password matches the stored hash"""
         user = self.db_manager.find_user_by_username(username)
         if user:
-            return bcrypt.check_password_hash(user.get('password'), password)
+            return self.bcrypt.check_password_hash(user.get("password"), password)
         return False
 
-class UserRegister(Resource):
-    def __init__(
-        self, data_validation: DataValidation, db_manager: DatabaseManager
-    ) -> None:
+
+class UserLogin(Resource):
+    def __init__(self, data_validation: DataValidation) -> None:
         self.data_validation = data_validation
-        self.db_manager = db_manager
 
     def post(self):
         try:
@@ -60,11 +55,40 @@ class UserRegister(Resource):
             if self.data_validation.data_complete(data):
                 username = data.get("username")
                 password = data.get("password")
-                if not self.data_validation.user_exists(username):
-                    hashed_password = bcrypt.generate_password_hash(password).decode(
-                        "utf-8"
+                if self.data_validation.verify_password(username, password):
+                    return make_response(jsonify({"message": "access granted"}), 200)
+                else:
+                    return make_response(
+                        jsonify({"message": "wrong username or password"}), 401
                     )
-                    if self.db_manager.insert_user(username, hashed_password):
+            else:
+                return make_response(
+                    jsonify({"message": "missing user or password"}), 400
+                )
+        except Exception as e:
+            print(f"Error: {e}")
+            return make_response(jsonify({"message": "something went wrong"}), 500)
+
+
+class UserRegister(Resource):
+    def __init__(self, data_validation: DataValidation) -> None:
+        self.data_validation = data_validation
+
+    def post(self):
+        try:
+            data = request.get_json()
+            if self.data_validation.data_complete(data):
+                username = data.get("username")
+                password = data.get("password")
+                if not self.data_validation.db_manager.find_user_by_username(username):
+                    hashed_password = (
+                        self.data_validation.bcrypt.generate_password_hash(
+                            password
+                        ).decode("utf-8")
+                    )
+                    if self.data_validation.db_manager.insert_user(
+                        username, hashed_password
+                    ):
                         return make_response(
                             jsonify({"message": "User registered successfully"}), 201
                         )
@@ -76,7 +100,7 @@ class UserRegister(Resource):
 
                 else:
                     return make_response(
-                        jsonify({"message": "User already exist"}), 409
+                        jsonify({"message": "User already exists"}), 409
                     )
 
             else:
@@ -84,15 +108,15 @@ class UserRegister(Resource):
                     jsonify({"message": "missing user or password"}), 400
                 )
 
-        except Exception:
+        except Exception as e:
+            print(f"Error: {e}")
             return make_response(jsonify({"message": "something went wrong"}), 500)
 
 
 db_manager = DatabaseManager()
-data_validation = DataValidation(db_manager)
-api.add_resource(
-    UserRegister, "/register", resource_class_args=(data_validation, db_manager)
-)
+data_validation = DataValidation(db_manager, bcrypt)
+api.add_resource(UserRegister, "/register", resource_class_args=(data_validation,))
+api.add_resource(UserLogin, "/login", resource_class_args=(data_validation,))
 
 
 if "__main__" == __name__:
